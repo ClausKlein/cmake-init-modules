0c3ff7bd42cb4692b3680a3d18eb5037d14ed540
ClausKlein <claus.klein@arcormail.de>
2024-09-29 11:00:37 +0200

    import-std-in-cmake-3-30

---
+++ std/main.cxx
@@ -0,0 +1,9 @@
+#include <string>
+
+void hello_world(std::string const& name);
+
+int main(int argc, char* argv[])
+{
+  hello_world(argv[0] ? argv[0] : "Voldemort?");
+  return 0;
+}
---
+++ std/uses_std.cxx
@@ -0,0 +1,6 @@
+import std;
+
+void hello_world(std::string const& name)
+{
+  std::cout << "Hello World! My name is " << name << std::endl;
+}
