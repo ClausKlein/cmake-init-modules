e1fba1d6fa4e41bbf479347646377b7c20d357b6
ClausKlein <claus.klein@arcormail.de>
2025-12-02 19:37:15 +0100

    make import std working

--- std/main.cxx
+++ std/main.cxx
@@ -1,9 +1,11 @@
 #include <string>
 
-extern void hello_world(std::string const& name);
+import none;
+
+// extern void hello_world(std::string const& name);
 
 auto main(int /*argc*/, char* argv[]) -> int
 {
-  hello_world((argv[1] != nullptr) ? argv[1] : "Voldemort?");
+  ::hello_world((argv[1] != nullptr) ? argv[1] : "Voldemort?");
   return 0;
 }
